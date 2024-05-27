0dcbdf9b512ab22a10c3f83f6516640565c79a9c
Alberto <alberto.garrote.ledesma.alu@iesjulioverne.es>
2024-05-27 11:02:41 +0200

    vrsion2

---
+++ priyetoPrueba/src/main/java/com/mycompany/priyetoprueba/PriyetoPrueba.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.priyetoprueba;
+
+/**
+ *
+ * @author alberto_garrote
+ */
+public class PriyetoPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
